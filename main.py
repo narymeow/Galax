5067df455f43b8bfda6eea7b46e7477fade6bc9d
Leonara (Nary) <61468299+narymeow@users.noreply.github.com>
2021-11-27 16:06:29 +0500

    1

--- main.py
+++ main.py
@@ -12,25 +12,19 @@ DISPLAY = pygame.display.set_mode(DISPLAY_SIZE)
 pygame.display.set_caption('Квадрат малевича') # название игры в консоли
 speed = 30
 user_x, user_y = DISPLAY_SIZE[0] // 2, DISPLAY_SIZE[1] // 2
-bg_img = pygame.image.load('Sources_img/backgroud.jpg')
-bg = bg_img.get_rect(center=(DISPLAY_SIZE[0] // 2, DISPLAY_SIZE[1] // 2))
+bg = pygame.image.load('Sources_img/background.jpg')
+dron = pygame.image.load('Sources_img/dron1.png')
 
 # Действия игры
 while GAME_STATUS:
-    bg.blit()
     DISPLAY.fill((255, 255, 255))
-    pygame.draw.rect(DISPLAY, (0, 0, 0), (user_x, user_y, 50, 50))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            GAME_STATUS = False
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            user_x -= speed
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 0:
-            user_x += speed
-        elif event.type == pygame.K_UP:
-            user_y -= speed
-        elif event.type == pygame.K_DOWN:
-            user_y += speed
+    DISPLAY.blit(bg, (100, 100))
+    DISPLAY.blit(dron, (user_x, user_y, 20, 20))
+    keys = pygame.mouse.get_pressed()
+    if keys[pygame.K_LEFT]:
+        user_x -= speed
+    elif keys[pygame.K_RIGHT]:
+        user_x += speed
     if user_x > DISPLAY_SIZE[0]:
         user_x = DISPLAY_SIZE[0] // 2
     elif user_x < 0:
